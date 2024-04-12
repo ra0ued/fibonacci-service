1e0a3ee5b36daea7a27c6b69728bf1e7e767b8cd
ra0ued <ra0ued@zabtech.ru>
2024-04-12 14:30:38 +0900

    Caching optimization

--- src/Application/Service/FibonacciService.php
+++ src/Application/Service/FibonacciService.php
@@ -5,11 +5,14 @@ declare(strict_types=1);
 namespace App\Application\Service;
 
 use PHP\Math\BigInteger\BigInteger;
+use Psr\Cache\InvalidArgumentException;
 use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Contracts\Cache\ItemInterface;
 
 class FibonacciService
 {
+    private const int CHUNK_SIZE = 100;
+    private const string SEPARATOR = ', ';
     private CacheInterface $cache;
 
     public function __construct(CacheInterface $cache)
@@ -17,22 +20,34 @@ class FibonacciService
         $this->cache = $cache;
     }
 
+    /**
+     * @throws InvalidArgumentException
+     */
     public function getFibonacci(int $from = 0, int $to = 1): ?string
     {
         if ($from < 0 || $to <= 0 || $to <= $from) {
             return null;
         }
 
-        $cacheKey = $from . '_' . $to;
+        $chunk = $this->calculateChunk($from, $to);
 
-        return $this->cache->get($cacheKey, function (ItemInterface $item) use ($from, $to): string {
+        $cacheKey = $chunk['start'] . '_' . $chunk['end'];
+
+        $cachedChunks = $this->cache->get($cacheKey, function (ItemInterface $item) use ($chunk): string {
             $item->expiresAfter(3600);
 
-            return $this->calculateFibonacci($from, $to);
+            $fibonacciNumbers = $this->calculateFibonacci($chunk['start'], $chunk['end']);
+
+            return implode(self::SEPARATOR, $fibonacciNumbers);
         });
+
+        $chunkArray = explode(self::SEPARATOR, $cachedChunks);
+        $rangedResult = array_slice($chunkArray, $from - $chunk['start'], ($to - $from) + 1);
+
+        return implode(self::SEPARATOR, $rangedResult);
     }
 
-    private function calculateFibonacci(int $from, int $to): string
+    private function calculateFibonacci(int $from, int $to): array
     {
         $firstNumber = 0;
         $secondNumber = 1;
@@ -46,8 +61,14 @@ class FibonacciService
             $secondNumber = $nextNumber;
         }
 
-        $result = array_slice($result, $from, ($to - $from) + 1);
+        return array_slice($result, $from, ($to - $from) + 1);
+    }
 
-        return implode(', ', $result);
+    private function calculateChunk(int $from, int $to): array
+    {
+        return [
+            'start' => (int)(ceil($from === 0 ? 1 : $from / self::CHUNK_SIZE) - 1) * self::CHUNK_SIZE,
+            'end' => (int)(ceil($to / self::CHUNK_SIZE)) * self::CHUNK_SIZE
+        ];
     }
 }
